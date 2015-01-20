c0161ff793c412b447503da90d9d17f72e6035e8
awtprod <awtprod@gmail.com>
2015-01-19 20:30:53 -0800

    db

--- app/config/database.php
+++ app/config/database.php
@@ -54,7 +54,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '127.0.0.1',
+			'host'      => '104.236.177.176',
 			'database'  => 'forge',
 			'username'  => 'forge',
 			'password'  => 'OZyXS27xCgjwNey9bull',
